85d492e23570b31ab18e910fb142e490c79211a7
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-08-03 15:09:01 +0200

    refactor: Add version14 theme to nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,16 @@
 return {
-  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "version14/nvim-theme",
+    name = "version14",
+    -- pinned to dark explicitly, rather than relying on the plugin's default
+    init = function()
+      vim.g.version14_style = "dark"
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "version14",
     },
   },
 }
